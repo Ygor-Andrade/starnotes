b2c63052a33b874a8bc37f9646b6d658660b8065
marcocarneiro <111753705+Luiiizzspk@users.noreply.github.com>
2022-11-30 09:26:30 -0300

    att no form

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -370,28 +370,30 @@
     <i class="fa fa-plus my-bt-float"></i>
   </a>
 
-  <div class="modal fade" id="modal-post" tabindex="-1" aria-labelledby="modal-postLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="modal-postLabel">Notas</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
+  <form>
+    <div class="modal fade" id="modal-post" tabindex="-1" aria-labelledby="modal-postLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="modal-postLabel">Notas</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
 
-       <div class="mb-3">
-         <label for="" class="form-label"></label>
-         <textarea class="form-control" name="nota" id="nota" rows="3"></textarea>
-       </div>
-
+        <div class="mb-3">
+          <label for="" class="form-label"></label>
+          <textarea class="form-control" name="nota" id="nota" rows="3"></textarea>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-          <button type="button" class="btn btn-primary">Salvar</button>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </form>
 
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
